b78bad8ab551f2abddbf07bc32d2adbd5e0dccb3
glescha <74572267+glescha@users.noreply.github.com>
2025-12-31 13:15:24 +0100

    Import installGlobalHaptics function

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./styles.css";
 import { getSetting, setSetting } from "./kv";
+import { installGlobalHaptics } from "./hapticsGlobal";
 
 getSetting("hapticsEnabled", null).then(v => {
   if (v === null) setSetting("hapticsEnabled", true);

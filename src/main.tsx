1e095f5a04f01379c0e45aa8d532fdb4a6b14ea1
glescha <74572267+glescha@users.noreply.github.com>
2025-12-31 13:07:06 +0100

    Add haptics setting initialization
    Check and set default for hapticsEnabled setting

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,9 @@
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./styles.css";
+import { getSetting, setSetting } from "./kv";
 
+getSetting("hapticsEnabled", null).then(v => {
+  if (v === null) setSetting("hapticsEnabled", true);
+});
 ReactDOM.createRoot(document.getElementById("root")!).render(<App />);
